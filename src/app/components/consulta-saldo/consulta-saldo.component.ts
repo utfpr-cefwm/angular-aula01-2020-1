8ac2f83eeefba97c0e3ba367a3bde8464697bd1e
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-09-11 15:04:14 +0000

    Cria um template de teste do `ConsultaSaldoComponent`.

--- src/app/components/consulta-saldo/consulta-saldo.component.ts
+++ src/app/components/consulta-saldo/consulta-saldo.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cliente } from 'src/app/models/cliente';
+
 @Component({
   selector: 'app-consulta-saldo',
   templateUrl: './consulta-saldo.component.html',
   styleUrls: ['./consulta-saldo.component.css'],
 })
 export class ConsultaSaldoComponent implements OnInit {
+
+  public cliente: Cliente = new Cliente(
+    'Francisco das Torres',
+    1.50,
+    new Date(),
+  );
 
   constructor(
   ) {
