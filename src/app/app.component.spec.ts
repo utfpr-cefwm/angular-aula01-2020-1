2b0441e5cfc292d70d590133c7195bccae92f656
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-09-11 15:03:41 +0000

    Chama o `ConsultaSaldoComponent` no `AppComponent`.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -12,6 +13,9 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
       ],
     }).compileComponents();
   });
