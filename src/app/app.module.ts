63cc6c7ee1a60d08e7dd00e65b4c3cde1474d93d
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-09-11 16:26:57 +0000

    Adiciona localização "pt-BR" à aplicação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConsultaSaldoComponent } from './components/consulta-saldo/consulta-saldo.component';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { ConsultaSaldoComponent } from './components/consulta-saldo/consulta-sal
     AppRoutingModule,
   ],
   providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
   ],
   bootstrap: [
     AppComponent,
